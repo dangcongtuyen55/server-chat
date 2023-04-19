2a4c708152028fa830ca5910e5a475c2c46e1cf1
dangcongtuyen55 <dangcongtuyen55@gmail.com>
2023-04-19 17:16:24 +0700

    update server

--- index.js
+++ index.js
@@ -13,14 +13,13 @@ app.use(bodyParser.json());
 
 // Configure open api
 const configuration = new Configuration({
-  organization: "org-VQ3EdogurBOeopgguVFiN7F3",
-  apiKey: open_api_key, // VISIT .env AND MAKE CHANGES
+  // organization: "PASTE YOUR ORGANIZATION ID HERE",
+  apiKey: process.env.open_api_key, // VISIT .env AND MAKE CHANGES
 });
 const openai = new OpenAIApi(configuration);
 
 // listeninng
-const PORT = 8000;
-app.listen(PORT, () => console.log("listening on port" + PORT));
+app.listen("3080", () => console.log("listening on port 3080"));
 
 // dummy route to test
 app.get("/", (req, res) => {
@@ -29,18 +28,16 @@ app.get("/", (req, res) => {
 
 //post route for making requests
 app.post("/", async (req, res) => {
+  const { message } = req.body;
+
   try {
-    const { message } = req.body;
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${message}`,
-      max_tokens: 30000,
+      max_tokens: 100,
       temperature: 0.5,
     });
     res.json({ message: response.data.choices[0].text });
-    // res.status(200).send({
-    //   message: response.data.choices[0].text,
-    // });
   } catch (e) {
     console.log(e);
     res.send(e).status(400);
